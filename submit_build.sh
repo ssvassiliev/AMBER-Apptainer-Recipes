e937058dae8804f797908e43a2b83c1d00420998
Serguei Vassiliev <svassili@login1.int.fir.alliancecan.ca>
2026-07-16 18:22:51 -0700

    fixed build with NCCL

--- submit_build.sh
+++ submit_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH -A def-svassili -c4 --mem=64000 --time=6:0:0
+#SBATCH -A def-svassili -c4 --mem=64000 --time=3:0:0
 
 module load apptainer
 
-apptainer build pmemd26-plumed-cuda.MPI.sif pmemd26-plumed-cuda.MPI.def
+apptainer build pmemd26_plumed2.10.1_cu12.8.1_ubuntu22.04.sif pmemd26_plumed2.10.1_cu12.8.1_ubuntu22.04.def
