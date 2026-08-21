fcdefb47535167208a1996889050e227274a9625
Serguei Vassiliev <svassili@login1.int.fir.alliancecan.ca>
2026-08-20 19:11:40 -0700

    fix path to sif

--- examples/ala-dipep-fullml-ani2x/submit.sh
+++ examples/ala-dipep-fullml-ani2x/submit.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-#SBATCH --nodes=1 -A def-svassili --ntasks 4 --gpus=nvidia_h100_80gb_hbm3_1g.10gb:1 --mem-per-cpu=4000   --time=10:0
+#SBATCH --nodes=1 -A def-svassili 
+#SBATCH --ntasks 4 --gpus=nvidia_h100_80gb_hbm3_1g.10gb:1 
+#SBATCH --mem-per-cpu=4000 --time=10:0
+
+SIF_PATH=$SCRATCH/AMBER-Apptainer-Recipes
 
 module load apptainer
-apptainer exec --nv /scratch/svassili/AMBER-Apptainer-Recipes/pmemd26_torchani.sif \
-	mpirun -n 4 pmemd.MPI -O  -c system.inpcrd  -p system.prmtop -i input.mdin 
+apptainer exec --nv $SIF_PATH/pmemd26_torchani-amber1e06f33_pt2.8.0_cu12.8.1_ubuntu24.04.sif \
+	mpirun -n $SLURM_NTASKS pmemd.MPI -O -c system.inpcrd -p system.prmtop -i input.mdin 
